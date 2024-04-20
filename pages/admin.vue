4bad66e7d9beba0718c7be1fb4fd665eb70eaf8b
Angelo Gabriel D. Evangelista <104245100+g-gelo@users.noreply.github.com>
2024-04-20 08:54:08 +0800

    Department read and add only

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,20 +1,7 @@
 <template>
   <div>
-    <NuxtLink
-      to="/department"
-      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-    >
-      Department
-    </NuxtLink>
-    Prisma ORM Data from the database, received
-    {{ departments?.length || 0 }} records:
-    <pre>{{ departments }}</pre>
-
-    <!-- <div v-for="MisnVis in departments" :key="MisnVis.id">
-      <h1>{{ MisnVis.department }}</h1>
-    </div> -->
     <!-- Form Modal -->
-    <!-- <div class="modal h-screen w-full">
+    <div class="modal h-screen w-full">
       <div class="modal-wrapper bg-white p-4 rounded-lg w-10/12">
         <h1 class="font-bold text-lg mb-4 mt-2">Report an Incident</h1>
         <form class="space-y-4" @submit.prevent="addDepartment(department)">
@@ -68,50 +55,48 @@
           </div>
         </form>
       </div>
-    </div> -->
+    </div>
   </div>
 </template>
 
 <script setup>
-const { data: departments } = useFetch("/api/departments");
-
 // Creating Report Incident Logic
-// const department = ref({
-//   departments: "",
-//   description: "",
-//   services: "",
-// });
+const department = ref({
+  departments: "",
+  description: "",
+  services: "",
+});
 
-// const addDepartment = async (department) => {
-//   try {
-//     const addedDepartment = await $fetch("/api/departments", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({
-//         department: department.department,
-//         description: department.description,
-//         services: department.services,
-//       }),
-//     });
-//     if (addedDepartment) {
-//       // Reset form fields
-//       department.department = "";
-//       department.description = "";
-//       department.services = "";
+const addDepartment = async (department) => {
+  try {
+    const addedDepartment = await $fetch("/api/departments", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        department: department.department,
+        description: department.description,
+        services: department.services,
+      }),
+    });
+    if (addedDepartment) {
+      // Reset form fields
+      department.department = "";
+      department.description = "";
+      department.services = "";
 
-//       // Update the list of report
-//       incidents.value = await $fetch("/api/departments");
-//     } else {
-//       console.error("Error adding report:", addedDepartment);
-//       // Handle the case where the report was not added successfully
-//     }
-//   } catch (error) {
-//     console.error("Error in addReport:", error);
-//     // Handle any other errors that might occur
-//   }
-// };
+      // Update the list of report
+      incidents.value = await $fetch("/api/departments");
+    } else {
+      console.error("Error adding report:", addedDepartment);
+      // Handle the case where the report was not added successfully
+    }
+  } catch (error) {
+    console.error("Error in addReport:", error);
+    // Handle any other errors that might occur
+  }
+};
 </script>
 
 <style scoped>
--- pages/department.vue
+++ pages/department.vue
@@ -4,21 +4,22 @@
     <div class="w-1/4 bg-gray-200 p-4">
       <!-- Clinical Department List -->
       <ul class="space-y-2">
-        <li class="p-2 border border-gray-300 hover:bg-gray-100 cursor-pointer">
-          Anesthesiology
+        <li
+          v-for="department in departments"
+          :key="department.id"
+          class="p-2 border border-gray-300 hover:bg-gray-100 cursor-pointer"
+          @click="selectDepartment(department)"
+        >
+          {{ department.department }}
         </li>
       </ul>
     </div>
 
     <!-- Clinical Department Contents -->
-    <div class="w-3/4 bg-white p-4">
+    <div v-if="selectedDepartment" class="w-3/4 bg-white p-4">
       <div class="flex justify-center">
         <!-- Logo -->
-        <img
-          src="/img/Doctor.svg"
-          alt="logo_img"
-          class="h-3 w-52 doctor-icon"
-        />
+        <img src="/img/Doctor.svg" alt="logo_img" class="w-52 doctor-icon" />
       </div>
 
       <div class="flex justify-center">
@@ -33,10 +34,7 @@
       <!-- Content Details -->
       <div class="py-4">
         <p class="px-4 py-2 border border-gray-300 rounded">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
+          {{ selectedDepartment.description }}
         </p>
       </div>
 
@@ -44,9 +42,7 @@
       <div class="py-4">
         <label class="block text-lg font-semibold">Services Offer:</label>
         <ul class="list-disc list-inside">
-          <li>Service 1</li>
-          <li>Service 2</li>
-          <li>Service 3</li>
+          <li>{{ selectedDepartment.services }}</li>
         </ul>
       </div>
     </div>
@@ -54,7 +50,12 @@
 </template>
 
 <script setup>
-//
+const { data: departments } = useFetch("/api/departments");
+const selectedDepartment = ref(null);
+
+const selectDepartment = (department) => {
+  selectedDepartment.value = department;
+};
 </script>
 
 <style scoped>
