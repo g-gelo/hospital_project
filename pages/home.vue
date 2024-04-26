0731490a24653dedfe71a8ef00597ca5e9b2d96e
Angelo Gabriel D. Evangelista <104245100+g-gelo@users.noreply.github.com>
2024-04-26 14:03:04 +0800

    DaisyUi Config

---
+++ pages/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button class="btn">Button</button>
+    <button class="btn btn-neutral">Neutral</button>
+    <button class="btn btn-primary">Primary</button>
+    <button class="btn btn-secondary">Secondary</button>
+    <button class="btn btn-accent">Accent</button>
+    <button class="btn btn-ghost">Ghost</button>
+    <button class="btn btn-link">Link</button>
+  </div>
+</template>
+
+<script setup>
+//
+</script>
+
+<style scoped>
+/*  */
+</style>
